d1900ca7c3c0e70bbf3322ad8de76c97fa4c9b78
dud5825@naver.com <dud5825@naver.com>
2024-10-17 15:32:04 +0900

    [조영민] 2024-10-17 -3강완료

---
+++ TypeTest/index2.js
@@ -0,0 +1,2 @@
+var userInfo = { 이름: "바보", 나이: 33, 출생지역: "북한" };
+var likeMusic = { song: "민물 장어의 꿈", singer: "신해철" };
---
+++ TypeTest/index2.ts
@@ -0,0 +1,2 @@
+let userInfo : {이름:string, 나이:number, 출생지역:string} = { 이름:"바보", 나이:33, 출생지역:"북한" };
+let likeMusic : { song : string, singer : string} = { song : "민물 장어의 꿈", singer : "신해철"};
