f6e4e2cb5190bc155fbe979b079afcf6fd695cff
dud5825@naver.com <dud5825@naver.com>
2024-10-17 16:54:56 +0900

    [조영민] 2024-10-17 -5강완료

--- TypeTest/index2.js
+++ TypeTest/index2.js
@@ -33,3 +33,58 @@ var 학교 = {
 };
 학교.score[4] = false;
 학교.friend = ['Lee', 학교.teacher];
+// 함수도 똑같이 타입 지정 해줘야됨
+function 함수2(x) {
+    return x * 2;
+}
+// 함수(30);
+// java void랑 똑같음
+function 함수3(x) {
+    x = 4;
+}
+// x? = number | undefined 랑 똑같음
+function 함수4(x) {
+    x = 4;
+}
+function 함수5(x) {
+    if (typeof (x) === "number") {
+        console.log(x + 4);
+    }
+    else {
+        console.log("숫자아님");
+    }
+    // x + 4;
+}
+함수5(123);
+함수5("ㅋㅋ");
+//숙제 1
+function 이름출력(x) {
+    if (x) {
+        console.log("안녕하세요" + x);
+    }
+    else {
+        console.log("이름이 없습니다.");
+    }
+}
+이름출력();
+//숙제 2
+function 자릿수세기(x) {
+    console.log(x.toString().length);
+    return x.toString().length;
+}
+자릿수세기(123123123);
+//숙제 3
+function 결혼중개사(money, house, charming) {
+    var score = 0;
+    if (house) {
+        score = score + 500;
+    }
+    if (charming === "상") {
+        score = score + 100;
+    }
+    score = score + money;
+    if (score >= 600) {
+        console.log("결혼가능");
+    }
+}
+결혼중개사(500, false, "상");
