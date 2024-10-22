89d1277524c82176834a86eb54988e1acba85f69
dud5825@naver.com <dud5825@naver.com>
2024-10-22 16:12:06 +0900

    [조영민] 2024-10-22 - 15강 완료

---
+++ TypeTest/index10.js
@@ -0,0 +1,28 @@
+// 함수 return 값에 붙일 수 있는 never type
+// 조건1. return 값이 없어야함
+// 조건2. endpoint가 없어야함
+// void와 유사함
+// 사실 함수는 return undefined 가 숨겨져있음 무조건
+// 사실 never타입 쓸모없음 ㅅㄱ
+// void 쓰면됨
+// never 타입은 진짜 말 안될때 등장함!
+function 네버리턴함() {
+    // 조건 1
+    // throw new Error()
+    // 조건 2
+    while (true) { }
+}
+// never 타입 등장하는 경우 1
+// 이상한 narrowing
+function 예시임(parameter) {
+    if (typeof parameter === 'string') {
+        console.log(parameter);
+    }
+    else {
+        console.log(parameter);
+    }
+}
+// never 타입이 등장하는 경우 2
+var 예시임2 = function () {
+    throw new Error();
+};
